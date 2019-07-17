564288d237a4018899c48f229b4a7d7b77c60a30
chaharshivam <chahar.shivam@yahoo.com>
2019-07-17 15:40:23 +0530

    Update script

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,15 +2,16 @@ const MD = document.querySelector('.mousedown');
 const MO = document.querySelector('.mouseover');
 const MV = document.querySelector('.mousemove');
 
+let rand = digits => Math.floor( Math.random() * Math.pow(10, digits) );
+
 MD.addEventListener('mousedown' , (e) => {
-    MD.style.background = `#${Math.floor( Math.random() * 1000000)}`;
+    MD.style.background = `#${rand(6)}`;
 });
 
 MO.addEventListener('mouseover', (e) => {
-    MO.style.background = 
-    `linear-gradient(to right, #${Math.floor( Math.random() * 1000000)},#${Math.floor( Math.random() * 1000000)}`;
+    MO.style.background = `linear-gradient(to right, #${rand(6)},#${rand(6)}`;
 });
 
 MV.addEventListener('mousemove', (e) => {
-    MV.style.background = `#${Math.floor( Math.random() * 1000000)}`;
+    MV.style.background = `#${rand(6)}`;
 });
