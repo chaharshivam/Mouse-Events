3739d42cb0148f7f27ccf6e882e1ea020a1d7a78
chaharshivam <chahar.shivam@yahoo.com>
2019-07-17 18:58:58 +0530

    Update script

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,9 @@
-const MD = document.querySelector('.mousedown');
-const MO = document.querySelector('.mouseover');
-const MV = document.querySelector('.mousemove');
+const MD    = document.querySelector('.mousedown');
+const MO    = document.querySelector('.mouseover');
+const MV    = document.querySelector('.mousemove');
+const CLK   = document.querySelector('.click');
+const DCLK  = document.querySelector('.doubleclick');
+const CNTXT = document.querySelector('.context');
 
 let rand = digits => Math.floor( Math.random() * Math.pow(10, digits) );
 
@@ -14,4 +17,16 @@ MO.addEventListener('mouseover', (e) => {
 
 MV.addEventListener('mousemove', (e) => {
     MV.style.background = `#${rand(6)}`;
+});
+
+CLK.addEventListener('click' , (e) => {
+    CLK.style.background = `#${rand(6)}`;
+});
+
+DCLK.addEventListener('dblclick' , (e) => {
+    DCLK.style.background = `#${rand(6)}`;
+});
+
+CNTXT.addEventListener('contextmenu' , (e) => {
+    CNTXT.style.background = `#${rand(6)}`;
 });
